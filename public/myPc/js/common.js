b3affbf517e73c24a8972a985be5c49c4c48a014
Your Name <494738945@qq.com>
2018-09-06 22:20:06 +0800

    首页公共部分完成

--- public/myPc/js/common.js
+++ public/myPc/js/common.js
@@ -19,4 +19,33 @@ $(function(){
       }
     })
   }
+
+
+  // 切换导航栏
+  $(".icon-menu").on("click",function(){
+    $(".lt_aside").toggleClass("toggle");
+    $(".lt_content").toggleClass("toggle");
+    $(".lt_content .head").toggleClass("toggle");
+  })
+
+
+  // 退出功能
+
+  $(".icon-logout").on("click",function(){
+    $("#logoutModal").modal("show");
+  })
+
+  $(".btn_logout").on("click",function(){
+    // console.log("哈哈")
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      success:function(data){
+        // console.log(data);
+        if(data.success){
+          location.href="login.html";
+        }
+      }
+    })
+  })
 })
