8b4ac80717bc2a1dc905e6651b51adfc42a3ed97
Your Name <494738945@qq.com>
2018-09-08 16:56:31 +0800

    二级分类页面完成

--- public/myPc/js/second.js
+++ public/myPc/js/second.js
@@ -13,7 +13,7 @@ function render(){
       pageSize:pageSize
     },
     success:function(data){
-      console.log(data);
+      // console.log(data);
       $("tbody").html(template("tpl",data));
       $(".page").bootstrapPaginator({
         bootstrapMajorVersion:3,
@@ -35,7 +35,117 @@ render();
 
 $(".addCat").on("click",function(){
   $("#addModal").modal("show");
+
+// 获取一级分类渲染下拉框
+  
+  $.ajax({
+    type:"get",
+    url:"/category/queryTopCategoryPaging",
+    data:{
+      page:1,
+      pageSize:100000000
+    },
+    success:function(data){
+      console.log(data);
+      $(".dropdown-menu").html(template("tpl2",data));
+    }
+  })
 })
+
+
+// 初始化表单验证
+
+$("#form").bootstrapValidator({
+  excluded:[],
+  feedbackIcons: {
+    valid: 'glyphicon glyphicon-ok',
+    invalid: 'glyphicon glyphicon-remove',
+    validating: 'glyphicon glyphicon-refresh'
+  },
+  fields:{
+    brandName:{
+      validators:{
+        notEmpty:{
+          message:"请选择一级分类名称"
+        }
+      }
+    },
+    brandLogo:{
+      validators:{
+        notEmpty:{
+          message:"请选择二级分类图片Logo"
+        }
+      }
+    },
+    categoryId:{
+      validators:{
+        notEmpty:{
+          message:"请选择一级分类名称"
+        }
+      }
+    }
+  }
+});
+
+// fileupload插件上传图片
+
+$("#fileUpload").fileupload({
+  dataType:"json",
+  done:function(e,data){
+    // result:{picAddr: "/upload/brand/e1db2270-b33a-11e8-97a3-6fe06a374081.jpg"}
+    // console.log(data.result.picAddr)
+    // 图片预览
+    $(".upImg").attr("src",data.result.picAddr);
+
+    // 将图片地址绑定给brandLogo
+    $("#brandLogo").val(data.result.picAddr);
+
+    $("#form").data("bootstrapValidator").updateStatus("brandLogo","VALID");
+
+
+  }
+})
+
+
+// 下拉框与categoryId的处理
+$(".dropdown-menu").on("click","a",function(){
+  // console.log($(this).text());
+  // console.log($(this).attr("data-id"));
+  $("#dropdownMenu1").text($(this).text());
+  $("input[name='categoryId']").val($(this).attr("data-id"));
+
+  $("#form").data("bootstrapValidator").updateStatus("categoryId","VALID");
+  
+})
+
+
+
+
+$("#form").on("success.form.bv",function(e){
+  e.preventDefault();
+
+
+  $.ajax({
+    type:"post",
+    url:"/category/addSecondCategory",
+    data:$("#form").serialize(),
+    success:function(data){
+      if(data.success){
+        render();
+        $("#form").data("bootstrapValidator").resetForm();
+        $("#form")[0].reset();
+        $(".upImg").attr("src","./img/default.png");
+        $("#dropdownMenu1").text("请选择");
+        $("#addModal").modal("hide");
+      }
+    }
+  })
+})
+
+
+
+
+
 
 
 
