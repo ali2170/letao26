2ea0b879a7d487d2e71eaabc1fa24f05fe5affab
Your Name <494738945@qq.com>
2018-09-12 17:16:26 +0800

    搜索详情页面完成

--- public/myMobile/js/searchList.js
+++ public/myMobile/js/searchList.js
@@ -23,7 +23,38 @@ $(function(){
   var obj = getSearch();
   var key = obj.key;
   console.log(key);
+  $("#search_txt").val(key);
 
+
+  function getHistory(){
+    var str = localStorage.getItem("lt_search_history") || "[]";
+    var arr = JSON.parse(str);
+    return arr;
+  }
+  $(".btn_search").on("click",function(){
+    var txt = $("#search_txt").val().trim();
+    if(!txt){
+      mui.toast("亲,你想买啥");
+      return;
+    }
+    var arr = getHistory();
+    var index = arr.indexOf(txt);
+    // console.log(index);
+
+    if(index != -1){
+      // alert("呵呵")
+      arr.splice(index,1);
+    }
+    
+    if(arr.length >= 10){
+      arr.pop();
+    }
+    arr.unshift(txt);    
+    localStorage.setItem("lt_search_history",JSON.stringify(arr));
+    render();
+
+    location.href = "searchList.html?key="+txt;
+  })
   // var option = {
   //   proName:key,
   //   page:1,
@@ -37,12 +68,19 @@ $(function(){
       data:{
         proName:key,
         page:1,
-        pageSize:10,
-        price:1,
-        num:1
+        pageSize:10
       },
       success:function(data){
         console.log(data);
+        setInterval(function(){
+          if(data.data.length == 0){
+            $(".lt_product ul").html("<p>没有更多数据了</p>");
+          }else{
+
+            $(".lt_product ul").html(template("tpl",data));          
+
+          }
+        },2000)
       },
       error:function(){
         console.log("出错了")
